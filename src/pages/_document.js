b542e632697be8ab71050d75b549728505c6f4a6
Andres Calle <andrescalleuribe@gmail.com>
2021-08-18 11:32:21 -0500

    meta facebook auth

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,7 @@ class MyDocument extends Document {
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossOrigin="anonymous" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="stylesheet" href="<NODE_MODULES_FOLDER>/react-responsive-carousel/lib/styles/carousel.min.css"/>
+          <meta name="facebook-domain-verification" content="4qf5st1dgsqq73nijfvnzq42oib9of" />
         </Head>
         <body>
           <Main />
